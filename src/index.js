f3a4ae5db69df15eada42633685e10d739c0a8c1
zhxgx <1454728991@qq.com>
2019-05-27 09:53:09 +0800

    添加状态管理

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import './assets/css/base.css' //引入公共样式
-import './library/font'
-
-import App from './layouts/App';
-
-import {BrowserRouter} from 'react-router-dom';
-  
 
 ReactDom.render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>,
+  <div>hello</div>,
   document.querySelector('#root')
 );
